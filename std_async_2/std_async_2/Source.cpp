d2fc848ab2dbeeab91bf26dc898d1be36ea91216
Ivaylo Donchev <idonchev@hotmail.com>
2019-12-28 14:54:42 +0200

    re

---
+++ std_async_2/std_async_2/Source.cpp
@@ -0,0 +1,22 @@
+#include <algorithm>
+#include <chrono>
+#include <functional>
+#include <future>
+#include <iostream>
+#include <thread>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+	auto fut1 = async([] {
+		for(int i=1; i<=100; i++)
+			cout << "1 ";
+		});
+	auto fut2 = async([] {
+		for(int i=1; i<=100; i++)
+			cout << "2 ";
+		});
+	cout << '\n';
+}
